a51fa13291b7b2962570c24d1265fb6b369d38e0
Anatoly Karlov <karleowne@gmail.com>
2023-08-14 09:25:05 +0300

    debug mode

--- src/main/java/dev/vality/exporter/walletbalances/config/OpenSearchClientConfig.java
+++ src/main/java/dev/vality/exporter/walletbalances/config/OpenSearchClientConfig.java
@@ -1,5 +1,10 @@
 package dev.vality.exporter.walletbalances.config;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.SneakyThrows;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
@@ -41,7 +46,12 @@ public class OpenSearchClientConfig {
 
     @Bean
     public OpenSearchClient openSearchClient(RestClient restClient) {
-        var transport = new RestClientTransport(restClient, new JacksonJsonpMapper());
+        var objectMapper = new ObjectMapper()
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .registerModule(new JavaTimeModule())
+                .configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true)
+                .setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        var transport = new RestClientTransport(restClient, new JacksonJsonpMapper(objectMapper));
         return new OpenSearchClient(transport);
     }
 
