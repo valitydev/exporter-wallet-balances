30fea660f72080f1c891daef570b101a5aab86b2
Anatoly Karlov <karleowne@gmail.com>
2023-08-14 17:18:58 +0300

    add registration in prometheus

--- src/main/java/dev/vality/exporter/walletbalances/service/WalletBalancesService.java
+++ src/main/java/dev/vality/exporter/walletbalances/service/WalletBalancesService.java
@@ -14,6 +14,7 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 @Slf4j
+@SuppressWarnings("LineLength")
 public class WalletBalancesService {
 
     private final OpenSearchService openSearchService;
